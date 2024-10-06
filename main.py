5584f94322d92de9edaaec4cd097113d08f9d262
TesrSK <davidkoscak1@gmail.com>
2024-10-06 11:09:43 +0200

    Asteroids are splitting now

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def main():
             for shot in shots:
                 if shot.collision(asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
                     
 
         dt = clock.tick(60) / 1000
